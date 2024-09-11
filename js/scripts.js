8dfdf79490ec8465db7f16a48d9bebb6fc686fef
dwyertyrell <Dwyertyrell@gmail.com>
2024-09-11 17:58:20 +0100

    Update scripts.js
    syntax error within the 2nd item of array

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 pokemonList = [
     {name: 'Charmeleon', type: ['fire'], height: 1.1},
-    {name:'Blastoise', type: [water], height: 1.6}, 
+    {name:'Blastoise', type: ['water'], height: 1.6}, 
     {name: 'Weedle', type: ['bug', 'poison'], height: 0.3}, 
     {name: 'Venusaur', type: ['grass', 'poison'], height: 2.0}
 ];
