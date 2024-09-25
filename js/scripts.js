297ff286d46420414a6999eecb1d5e0941ff7490
dwyertyrell <Dwyertyrell@gmail.com>
2024-09-25 19:09:27 +0100

    Update scripts.js
    Replaced the for loop  with the higher order forEach() function - for better functional programming.
    
    added comments to keep track of my thought- pattern

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,22 @@ let pokemonList = [
     {name: 'Venusaur', type: ['grass', 'poison'], height: 2.0}
 ];
 
-for ( let i= 0; i < pokemonList.length; i++) {
-    
-    document.write( '<p>' + pokemonList[i].name + ' ' +'('+' ' + pokemonList[i].height + ' ' + ')' + '</p>');
+pokemonList.forEach(function(creature) {;
+    // althought the object is inside the array, it's okay- because the forEach will give us individaul 
+    //  access to each element withing the array, through iteration.
+    // but how to access the properties of the objects? 
+    document.write( '<p>' + creature.name + ' ' +'('+' ' + creature.height + ' ' + ')' + '</p>');
 
-    if (pokemonList[i].height > 1.5) {
-        document.write( pokemonList[i].name + '-' + ' ' + 'Wow, this pokemon is tall!' + ' ');
-    }
-}
+    if (creature.height > 1.5) {
+            document.write(creature.name + '-' + ' ' + 'Wow, this pokemon is tall!' + ' '); 
+        }
+    });
+
+// for ( let i= 0; i < pokemonList.length; i++) {
+    
+//     document.write( '<p>' + pokemonList[i].name + ' ' +'('+' ' + pokemonList[i].height + ' ' + ')' + '</p>');
+
+    // if (pokemonList[i].height > 1.5) {
+    //     document.write( pokemonList[i].name + '-' + ' ' + 'Wow, this pokemon is tall!' + ' ');
+//     }
+// }
