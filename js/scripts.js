3e0f98261d6dbd770aa23c78ae00133d10f8197a
dwyertyrell <Dwyertyrell@gmail.com>
2024-10-01 16:36:57 +0100

    updated scripts.js
    Overall, made the addListItem() function more dynamically available within the file:
    
    adding a reference to its function
    inserting the entire function within the IIFE, leaving only a reference behind, in the forEach().
    
    To access this function, another key-value pair was created in the return object, referencing addListItem function

--- js/scripts.js
+++ js/scripts.js
@@ -18,9 +18,30 @@ let pokemonRepository = (function() {
         return pokemonList
     }
 
+    function addListItem(pokemon) {
+        let unOList = document.querySelector('.pokemon-list');
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add('button');
+        // append the button to the list item as a child
+        listItem.appendChild(button);
+        // append the listItem to the unordered lists as its child 
+        unOList.appendChild(listItem);
+        button.addEventListener('click', function(pokemon) {
+            showDetails(pokemon);
+        })
+
+    }
+
+    function showDetails(pokemon) {
+        console.log(pokemon);
+
+    }
     return {
         add: add,
         getAll: getAll,
+        addListItem: addListItem
     };
 })();
 
@@ -28,22 +49,21 @@ pokemonRepository.add({name: 'Ekans', type: ['poison'], height: 2});
 console.log(pokemonRepository.getAll());
 
 // we are accessing each object as an element within the array. Therefore, the parameter of the forEach()
-// acting on the array, can be used as a reference point for the objects- via dot notation.   
+// acting on the array, can be used as a reference point for the objects- via dot notation. 
+
+// the parameter is also referencing the same object of the parameter: pokemon. 
+// therefore can replace creature with pokemon- no need to have two names for the same reference. 
 let list = pokemonRepository.getAll();
 
-// why did JavaScript make me use an intermediate variable to work with the forEach()? 
-list.forEach(function(creature) {
+list.forEach(function(pokemon) {
+    // calling the addListItem function into my for loop. this one line code simplifies 
+    // everything-since all instructions are dynamically stored in a function.  
+    pokemonRepository.addListItem(pokemon);
 
+// since the 1st variable to be declared as pokemonList is inside the IIFE, could i use the 
+// name again for a completely diffferent variable? it looks like this will 
+// help me understand the logic behind my code 
 
-    let unOList = document.querySelector('.pokemon-list');
-    let listItem = document.createElement('li');
-    let button = document.createElement('button');
-    button.innerText = creature.name;
-    button.classList.add('button');
-    // append the button to the list item as a child
-    listItem.appendChild(button);
-    // append the listItem to the unordered lists as its child 
-    unOList.appendChild(listItem)
 
     });
     
