66762d92bafda9f64fb617ad927fef4f5c6539f1
dwyertyrell <Dwyertyrell@gmail.com>
2024-10-04 13:00:05 +0100

    Update scripts.js
    removed the data which was manually add inside the array.
    data from an API will be fetched and added into the array, later on.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,6 @@
 
 let pokemonRepository = (function() {
-    let pokemonList = [
-        {name: 'Charmeleon', type: ['fire'], height: 1.1},
-        {name:'Blastoise', type: ['water'], height: 1.6}, 
-        {name: 'Weedle', type: ['bug', 'poison'], height: 0.3}, 
-        {name: 'Venusaur', type: ['grass', 'poison'], height: 2.0}
-    ];
+    let pokemonList = [];
 //  difference between my creature parameter, and my pokemon parameter?
     function add(pokemon){
 
