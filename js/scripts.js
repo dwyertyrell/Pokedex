174ff2664fe85ca4d6f734283defa6f146894b68
dwyertyrell <Dwyertyrell@gmail.com>
2024-10-09 12:00:30 +0100

    Update scripts.js
    removed comment.
    
    provided a more suitable name for a variable.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 let pokemonRepository = (function() {
     let pokemonList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?imit=150';
-//  difference between my creature parameter, and my pokemon parameter?
+
     function add(pokemon){
 
         if (typeof(pokemon) === 'object') {
@@ -15,7 +15,7 @@ let pokemonRepository = (function() {
     }
 
     function addListItem(pokemon) {
-        let unOList = document.querySelector('.pokemon-list');
+        let pokemonList = document.querySelector('.pokemon-list');
         let listItem = document.createElement('li');
         let button = document.createElement('button');
         button.innerText = pokemon.name;
@@ -23,7 +23,7 @@ let pokemonRepository = (function() {
         // append the button to the list item as a child
         listItem.appendChild(button);
         // append the listItem to the unordered lists as its child 
-        unOList.appendChild(listItem);
+        pokemonList.appendChild(listItem);
         // avoid placing a parameter in the addEventListener- as the parameter found in the 
         // code block is assigned to a different variable.  
         button.addEventListener('click', function() {
