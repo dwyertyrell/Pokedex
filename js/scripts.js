e9d496bda354b0798bcd9bcfc046c8b5afd6dcef
dwyertyrell <Dwyertyrell@gmail.com>
2024-10-22 18:53:55 +0100

    updated scripts.js
    updated the selection of the '#modalContainer'.
    
    incorporated Bootstrap's css classes onto the dynamically created elements in addListItem()- giving the <button > a data-toggle attribute to open Bootstrap's UI modal component.
    
    Removed an older version of the showModal() which didn't incorporated Bootstrap's UI modal component.
    
    added an extra object property, created dynamically in loadDetails(), to access the pokemon's type(s).
    
    used jQuery syntax to create newly added the show Modal() function.
    
    added a general function to hide the modal- using this as a reference to the three event handlers to close the modal.
    
    removed old comments/ commented code, throughout the file.
    
    updated the argument of the showModal() on line 157.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,8 @@
 let pokemonRepository = (function() {
     let pokemonList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?imit=150';
-    let modalContainer = document.querySelector('#modal-container');
+    let modalContainer = document.getElementById('modal-container');
+
 
     function add(pokemon){
 
@@ -18,9 +19,13 @@ let pokemonRepository = (function() {
     function addListItem(pokemon) {
         let pokemonList = document.querySelector('.pokemon-list');
         let listItem = document.createElement('li');
+        listItem.classList.add('list-group-item');
         let button = document.createElement('button');
         button.innerText = pokemon.name;
         button.classList.add('button');
+        button.classList.add('btn');
+        button.classList.add('btn-light');
+        button.setAttribute('data-toggle','modal');
         // append the button to the list item as a child
         listItem.appendChild(button);
         // append the listItem to the unordered lists as its child 
@@ -33,54 +38,92 @@ let pokemonRepository = (function() {
 
     }
 
-    function showModal(title, text, img) {
-    
-        let pokemonList = document.querySelector('.pokemon-list')
-        let modal = document.createElement('div');
-        modal.classList.add('modal');
-
-        // this line of code clears the modal of the pokemon in the
-        // previous 'click' event. 
-        modalContainer.innerHTML ='';
-
-        
-        let closeButton = document.createElement('button');
-        closeButton.classList.add('close-Button');
-        closeButton.innerText = 'close';
-        closeButton.addEventListener('click', hideModal);
-
-        let pokemonName = document.createElement('h1');
-        pokemonName.classList.add('title');
-        pokemonName.innerText = title;
-
-        let pokemonHeight = document.createElement('p');
-        pokemonHeight.classList.add('text');
-        pokemonHeight.innerText = text;
-
-        let pokemonImage = document.createElement('img');
-        // can use a 'src' as a property, as well as using it as an 
-        // attribute.
-        // why did i have to use the parameter img as value, 
-        // and not item.imageUrl?
-        pokemonImage.src = img;
-
-        modal.appendChild(closeButton);
-        modal.appendChild(pokemonName);
-        modal.appendChild(pokemonHeight);
-        modal.appendChild(pokemonImage);
-        modalContainer.appendChild(modal);
-        pokemonList.appendChild(modalContainer);
-
-        modalContainer.classList.add('is-visible');
-
-        modalContainer.addEventListener('click', (e) => {
-            let target = e.target;
-            if(target === modalContainer) {
-                hideModal();
-            }
-        });
-    
+    function loadList() {
+        return fetch(apiUrl).then (function (response) {
+            // if fetch is successful, then parse json data 
+            // into a js object
+            return response.json();
+        }).then( function(json) {
+            // if the parse is successful, then iterate through the 'result'
+            // key of the json object. 
+            json.results.forEach(function(item) {
+            // due to the forEach(), its parameter 'item' is referencing each element within 
+            // the json.results (AN ARRAY) the 'item' is an object and 
+            // the name is a key of the 'item' object.
+                let pokemon = {
+                    // an object is created as the forEach() passes each element.
+                    // using the values of the 'item's value. 
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+            });
+        }).catch(function (e) {
+            console.error(e)
+            });
     }
+
+    function loadDetails(item) { 
+        // 'item' = pokemon object
+        // this function: loadDetails() is using the data that was acted upon
+        // by the last code of loadList(). therefore, 'item' refers to the pokemon object 
+        // in the add() argument- the last code 
+        let url = item.detailsUrl;
+        // url is the pokemon.detailsUrl in the previous function.
+        // this is a link to another api containing a json object- 
+        // therefore need to parse again. 
+        // url = pokemon.detailsUrl
+        return fetch(url).then(function(response){
+            return response.json();
+        // 'response' = parsed pokemon.detailsUrl (an entire js object now)
+        }).then(function(details){
+            // details is the 
+            //  here, we created two keys, dynamically. 
+            item.imageUrl = details.sprites.front_default;
+            item.height = details.height;
+            item.imageBack = details.sprites.back_default;
+            item.imageShiny = details.sprites.front_shiny;
+            item.type = details.types.map((element) => element.type.name);
+           
+            
+        }).catch(function(e) {
+            console.error(e);
+        });
+    }
+    
+// parameter is representing the pokemon.
+// i replaced the parameter 'item' for 'pokemon' since it wasn't defined.
+  function showModal(item) {
+    let modalBody = $('.modal-body');
+    let modalTitle= $('.modal-title');
+    // let modalHeader= $('.modal-header');
+
+    modalTitle.empty();
+    modalBody.empty();
+
+    let nameElement = $('<h1>'+ item.name +'</h1>');
+    
+    let imageElementFront = $('<img class="modal-img" style="width: 30%">');
+    imageElementFront.attr('src', item.imageUrl);
+    let imageElementBack = $('<img class= "modal-img" style= "width: 30%">');
+    imageElementBack.attr('src', item.imageBack);
+    let imageElementShiny = $('<img class= modal-img style= "width: 30%">');
+    imageElementShiny.attr('src', item.imageShiny);
+
+    let heightElement = $('<p>' + 'Height :' + item.height + '</p>');
+    let typeElement = $('<p>' + 'Type(s) :' + item.type +'</p>'); 
+
+    modalTitle.append(nameElement);
+    modalBody.append(imageElementFront);
+    modalBody.append(imageElementBack);
+    modalBody.append(imageElementShiny);
+    modalBody.append(heightElement);
+    modalBody.append(typeElement);
+    
+    // FEEDBACK CODE
+    modalContainer.classList.add('is-visible');
+    modalContainer.classList.add('show');
+  };
 
     function hideModal(){
     modalContainer.classList.remove('is-visible');
@@ -92,49 +135,26 @@ let pokemonRepository = (function() {
         };
     });
 
-    function loadList() {
-        return fetch(apiUrl).then (function (response) {
-            return response.json();
-        }).then( function(json) {
-            json.results.forEach(function(item) {
-                let pokemon = {
-                    // values of the keys are being referenced by
-                    // the values of the keys in the result object of the json file
-                    name: item.name,
-                    detailsUrl: item.url
-                };
-                add(pokemon);
-            });
-        }).catch(function (e) {
-            console.error(e)
-            });
-    }
+// needs to be more specific to prevent closing the modal 
+// if the user clicked on the modal itself. 'e' is the event object.
+    modalContainer.addEventListener('click', (e) => {
+        let target = e.target;
+        if(target === modalContainer) {
+            hideModal();
+        }
+    });
 
-    function loadDetails(item) {
-    //   variable assigned to the key-value pair "url", in the result object 
-    // of the json file. 
-        let url = item.detailsUrl;
-        return fetch(url).then(function(response){
-            return response.json();
-        }).then(function(details){
-            item.imageUrl = details.sprites.front_default;
-            item.height = details.height;
-            item.type = details.types;
-        }).catch(function(e) {
-            console.error(e);
-        });
-    }
-
-
-    // replace the console log in showDetails with the showModal()
-    // within its parameters, add the corresponding key-values for their pokemon Image,
-    // pokemon Height, and pokemon Name.
+// using jQuery syntax to add an event listener on the 
+// modal's closing button
+    $('.close').on('click', (e) => {
+        if(modalContainer) {
+            hideModal();
+        }
+    });
 
     function showDetails(pokemon) {
         loadDetails(pokemon).then(function() {
-            // these arguments are accessing the pokemon's object [created in 
-            // the loadList()] but through the loadDetails().
-            showModal(pokemon.name, pokemon.height, pokemon.imageUrl);
+            showModal(pokemon);
         })
     }
     
